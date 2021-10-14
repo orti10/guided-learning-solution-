9ccb9cd1f334fe9d061510e5d071318bdd811f3f
orti10 <ortalh10@gmail.com>
2021-10-14 14:19:34 +0300

    guide window includes buttons completed :happy:

--- player.js
+++ player.js
@@ -1,4 +1,10 @@
-const GUIDED_LEARNING_DATA_URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?";
+// Building a guided learning frontend engine
+// player.js that will eventually run a guide on google.com 
+// Written by Ortal
+
+const GUIDED_URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?";
+// The url is the same as:
+//https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
 const ActionType = {
     TIP: 'tip',
     CLOSESCENARIO: 'closeScenario'
@@ -17,27 +23,35 @@ const Placement = {
 let steps;
 let currStepIndex = 0;
 
+function  loadCss(){
+     // injecting css
+     let styleTag = document.createElement('link');
+     // Add attributes
+     styleTag.rel = 'stylesheet';
+     styleTag.href = 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css';
+     // Attach to the document head
+     document.head.appendChild(styleTag);
+     console.log("CSS loaded successfully!")
+}
+
 // Adding tool tip from css link using jQuery
 // window.jQuery is object defined in window global object
 // Pulling from an article that was linked to in a comment
 function addToolTip(tooltipCss, tip){
-    var styleTag = document.createElement('link');
-    styleTag.rel = 'stylesheet';
-    // Injecting the following css to the page to make the tooltips look good
-    styleTag.href = 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css';
-    document.head.appendChild(styleTag);
+    loadCss();
 
     // Using the below wrapper to place the tip contents
-    (window.jQuery)(document.head).append("<style>" + tooltipCss + "</style>");
+    (window.jQuery)(document.head).append(
+        "<style>" + tooltipCss + "</style>");
     (window.jQuery)(document.body).append(
         "<div  class='sttip'> " +
-        "<div class='tooltip in'> " +
-        "<div class='tooltip-arrow'></div>" +
-        "<div class='tooltip-arrow second-arrow'></div>" +
-        "<div class='popover-inner'>" +
-        tip +
-        "</div>" +
-        "</div>" +
+            "<div class='tooltip in'> " +
+                "<div class='tooltip-arrow'></div>" +
+                "<div class='tooltip-arrow second-arrow'></div>" +
+                "<div class='popover-inner'>" +
+                    tip +
+                "</div>" +
+            "</div>" +
         "</div>");
     (window.jQuery)("span[data-iridize-role='stepsCount']").text(steps.length);
     (window.jQuery)("button[data-iridize-role='prevBt']").css({padding: '0 5px'});
@@ -76,8 +90,8 @@ function setTooltipPlacement(){
         }
         case Placement.BOTTOM:{
             tooltipPosition = getTooltipPosition(selectPositions.bottom,
-                (selectPositions.right + selectPositions.left) / 2,
-                selectPositions);
+            (selectPositions.right + selectPositions.left) / 2,
+            selectPositions);
             break;
         }
     }
@@ -86,24 +100,74 @@ function setTooltipPlacement(){
 
 function getTooltipPosition(top, left, selectPositions){
     const bodyPosition = (window.jQuery)(document.body)[0].getBoundingClientRect();
-    if (left + TOOLTIP_WIDTH > bodyPosition.right){
+    if(left + TOOLTIP_WIDTH > bodyPosition.right){
         left = selectPositions.left - TOOLTIP_WIDTH;
     }
-    if (top + TOOLTIP_HEIGHT > bodyPosition.bottom){
+    if(top + TOOLTIP_HEIGHT > bodyPosition.bottom){
         top = selectPositions.top - TOOLTIP_HEIGHT;
     }
     return {top: top, left: left}
 }
 
+function nextClick(){
+    const nextStepId = steps[currStepIndex].followers[0].next;
+    currStepIndex = steps.findIndex(val => val.id === nextStepId);
+    addTooltipContent();
+    if(currStepIndex > 0){
+        (window.jQuery)("button[data-iridize-role='prevBt']").removeClass('default-prev-btn');
+    }
+    if(currStepIndex === steps.length - 1){
+        (window.jQuery)("a[data-iridize-role='nextBt']").css({display:'none'})
+    }
+}
+
+function backClick(){
+    currStepIndex--;
+    if(currStepIndex === 0){
+        (window.jQuery)("button[data-iridize-role='prevBt']").addClass('default-prev-btn');
+    }
+    if(currStepIndex < steps.length - 1){
+        (window.jQuery)("a[data-iridize-role='nextBt']").css({display:'block'})
+    }
+    addTooltipContent();
+}
+
+function closeTooltip(){
+    (window.jQuery)('.sttip').css({display:'none'});
+}
 
 function addTooltipContent(){
     (window.jQuery)("span[data-iridize-role='stepCount']").text(currStepIndex + 1);
     const action = steps[currStepIndex].action;
     let content;
-    if (action.type === ActionType.TIP){
+    if(action.type === ActionType.TIP){
         content = action.contents["#content"];
     } else{
-        content = '<p>Bravo! You have completed the guide!</p>';
+        content = '<p>Bravo! You have completed the guide.</p>';
     }
+    setTooltipPlacement();
     (window.jQuery)("div[data-iridize-id='content']").html(content);
 }
+
+function getSteps(){
+    (window.jQuery).getJSON(
+        GUIDED_URL,
+        (json) => {
+            let jsonData = json.data;
+            steps = jsonData.structure.steps;
+            if(steps && steps.length > 0){
+                addToolTip(jsonData.css, jsonData.tiplates.tip);
+            }
+        }
+    );
+}
+
+// Loading and Injecting Jquery
+(function startGuide(){
+    var scriptTag = document.createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+    document.head.appendChild(scriptTag);
+    scriptTag.onload = getSteps;
+    console.log("jQuery loaded successfully!");
+})()
